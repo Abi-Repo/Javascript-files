a81b872d4c6f2f0e08ce2bd085ad3be0d94a88c0
abi <abinayainwork@gmail.com>
2025-12-29 18:26:54 +0530

    datastructures

---
+++ datastructures.js
@@ -0,0 +1,59 @@
+//array
+// var arr = ['abi', 'akshsaya','nithya','sush']
+// console.log(arr);
+// console.log(arr[1])
+// console.log(arr[3])
+
+//eg for array
+// var abi = [1,2,4,2,341,34]
+// for(let i= 0; i < abi.length ; i++){
+//     console.log(abi[i]);
+// }
+
+//Find the sum of all elements in an array.
+// var abi = [1,2,3,4,5]
+// var temp = 0;
+// for(let i = 0; i < abi.length; i++){
+//     temp = temp + abi[i];
+// }
+// console.log(temp);
+
+
+//Remove duplicates from an array without using Set
+// var arr = [1,2,34,4,1,2,3,4];
+// let temp = []
+// for(let i = 0; i < arr.length; i++){
+//     if(!temp.includes(arr[i])){
+//         temp.push(arr[i]);
+//     }
+// }
+// console.log(temp);
+
+
+// //array constructor  - like creating on our own 
+// var newarray = new Array;
+// newarray[0] = 'abi';
+// newarray[1] = 'nithya';
+// newarray[2] = 'akshu';
+// console.log(newarray);
+
+
+// //object 
+// let student = {
+//     name : 'abi',
+//     age : 19,
+//     course : 'aids'
+// }
+// console.log(student.name);
+// console.log(student.age);
+// console.log(student.course);
+
+//another example for object 
+// let student = {
+//     "name" : 'abi',
+//     'age' : 19
+// }
+// console.log(student.name);
+// console.log(student['name']);
+// console.log(student.age)
+// console.log(student);
